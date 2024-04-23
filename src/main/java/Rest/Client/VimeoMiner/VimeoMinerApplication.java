73741d50af5e109f3419eecce3fdad975f06715d
15M4C4R <mkhazni2004@gmail.com>
2024-04-23 11:30:56 +0200

    nuevo

---
+++ src/main/java/Rest/Client/VimeoMiner/VimeoMinerApplication.java
@@ -0,0 +1,13 @@
+package Rest.Client.VimeoMiner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VimeoMinerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VimeoMinerApplication.class, args);
+	}
+
+}
