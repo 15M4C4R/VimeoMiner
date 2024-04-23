661a99eacc74d0b3f29be8e4d1043fdc36c7ce71
15M4C4R <mkhazni2004@gmail.com>
2024-04-23 11:58:17 +0200

    nuevo

---
+++ src/main/java/Rest/Client/VimeominerApplication.java
@@ -0,0 +1,18 @@
+package Rest.Client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class VimeominerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VimeominerApplication.class, args);}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) { return builder.build(); }
+
+}
