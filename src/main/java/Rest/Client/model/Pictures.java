b4f8347f8a2547fb1462383fed363fc1b97ac2b8
15M4C4R <mkhazni2004@gmail.com>
2024-04-24 09:15:50 +0200

    channel

---
+++ src/main/java/Rest/Client/model/Pictures.java
@@ -0,0 +1,24 @@
+package Rest.Client.model;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+
+public class Pictures {
+
+    @JsonProperty("base_link")
+    private String baseLink;
+
+    @JsonProperty("base_link")
+    public String getBaseLink() {
+        return baseLink;
+    }
+
+    @JsonProperty("base_link")
+    public void setBaseLink(String baseLink) {
+        this.baseLink = baseLink;
+    }
+
+}
---
+++ src/main/java/Rest/Client/model/User.java
@@ -0,0 +1,111 @@
+
+package Rest.Client.model;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+
+public class User {
+
+    @JsonProperty("uri")
+    private String uri;
+    @JsonProperty("name")
+    private String name;
+    @JsonProperty("link")
+    private String link;
+    //private String user_link = getLink();
+    @JsonProperty("pictures")
+    private Pictures pictures;
+
+    private String picture_link = pictures.getBaseLink();
+
+    private String id = List.of(this.uri.split("/")).get(-1);
+
+    @JsonProperty("uri")
+    public String getUri() {
+        return uri;
+    }
+
+    @JsonProperty("uri")
+    public void setUri(String uri) {
+        this.uri = uri;
+    }
+
+    @JsonProperty("name")
+    public String getName() {
+        return name;
+    }
+
+    @JsonProperty("name")
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @JsonProperty("link")
+    public String getLink() {
+        return link;
+    }
+
+    @JsonProperty("link")
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    @JsonProperty("pictures")
+    public Pictures getPictures() {
+        return pictures;
+    }
+
+    @JsonProperty("pictures")
+    public void setPictures(Pictures pictures) {
+        this.pictures = pictures;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id =id;
+    }
+
+    public String getPicture_link() {
+        return this.picture_link;
+    }
+
+    public void setPicture_link(String picture_link) {
+        this.picture_link = picture_link;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(User.class.getName()).append('@').append(Integer.toHexString(System.identityHashCode(this))).append('[');
+        sb.append("uri");
+        sb.append('=');
+        sb.append(((this.uri == null)?"<null>":this.uri));
+        sb.append(',');
+        sb.append("name");
+        sb.append('=');
+        sb.append(((this.name == null)?"<null>":this.name));
+        sb.append(',');
+        sb.append("link");
+        sb.append('=');
+        sb.append(((this.link == null)?"<null>":this.link));
+        sb.append(',');
+        sb.append("pictures");
+        sb.append('=');
+        sb.append(((this.pictures == null)?"<null>":this.pictures));
+        sb.append(',');
+        if (sb.charAt((sb.length()- 1)) == ',') {
+            sb.setCharAt((sb.length()- 1), ']');
+        } else {
+            sb.append(']');
+        }
+        return sb.toString();
+    }
+
+}
