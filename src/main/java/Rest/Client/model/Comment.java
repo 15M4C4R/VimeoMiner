e352543b0cba5523781da09cba89d14ece4c21ff
15M4C4R <mkhazni2004@gmail.com>
2024-04-24 09:36:52 +0200

    dd

--- src/main/java/Rest/Client/model/Comment.java
+++ src/main/java/Rest/Client/model/Comment.java
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class Comment {
+public class Comment {md
     @JsonProperty("uri")
     private String uri;
 
